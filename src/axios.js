d29bab4802f29e6feaabd3f40791b4fc10ff3146
denis-snitko <denissnitko@gmail.com>
2023-01-14 14:09:25 +0300

    Update api

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const { REACT_APP_API } = process.env;
+const { REACT_APP_API, REACT_APP_API_PREFIX } = process.env;
+
+console.log(`${REACT_APP_API}${REACT_APP_API_PREFIX}`);
 
 const instance = axios.create({
-  baseURL: REACT_APP_API,
+  baseURL: `${REACT_APP_API}${REACT_APP_API_PREFIX}`,
 })
 
 instance.interceptors.request.use((config) => {
