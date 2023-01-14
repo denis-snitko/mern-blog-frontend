705df8ebe3a448fe860952192641d31ba8f0697e
denis-snitko <denissnitko@gmail.com>
2023-01-14 20:57:35 +0300

    Add tags route

---
+++ src/pages/Tags.jsx
@@ -0,0 +1,67 @@
+import { useEffect } from "react";
+import Grid from "@mui/material/Grid";
+
+import { Post } from "../components";
+import { fetchPosts } from "../redux/slices/posts";
+import { useDispatch, useSelector } from "react-redux";
+import { userData } from "../redux/slices/auth";
+import { Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+export const Tags = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(userData);
+  
+  const { posts } = useSelector((state) => state.posts);
+  
+  const { tag } = useParams();
+  
+  const isPostLoading = posts.status === "loading";
+  
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+  
+  if (isPostLoading) {
+    return [...Array(5)].map(((item, index) =>
+      <Post key={index} isLoading={isPostLoading} />));
+  }
+  
+  if (posts.items.length === 0) {
+    return <Typography variant="h4" component="div">Нет статей по тэгу
+      <Typography variant="h4" component={"span"}
+        fontWeight={700}>&#171;{tag}&#187;</Typography>
+    </Typography>;
+  }
+  
+  return (
+    <>
+      <Typography variant="h4" component="div" mb={4}>
+        Результаты по тэгу <Typography variant="h4" component={"span"}
+        fontWeight={700}>&#171;{tag}&#187;</Typography>
+      </Typography>
+      <Grid container spacing={4}>
+        <Grid xs={12} item>
+          {posts.items.map((post) =>
+            <Post
+              key={post._id}
+              _id={post._id}
+              title={post.title}
+              imageUrl={post.imageUrl}
+              author={{
+                _id: post.author._id,
+                avatarUrl: post.author.avatarUrl,
+                fullName: post.author.fullName
+              }}
+              createdAt={post.createdAt}
+              viewsCount={post.viewsCount}
+              commentsCount={3}
+              tags={post.tags}
+              isEditable={user?._id === post.author._id}
+            />
+          )}
+        </Grid>
+      </Grid>
+    </>
+  );
+};
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { AddPost, FullPost, Home, Login, Registration } from "../pages";
 import { Private } from "../hoc/Private";
 import { AppLayout } from "../components/AppLayout";
+import { Tags } from "../pages/Tags";
 
 export const router = createBrowserRouter([
   {
@@ -12,7 +13,8 @@ export const router = createBrowserRouter([
       { path: "register", element: <Registration /> },
       { path: "create", element: <Private><AddPost /></Private> },
       { path: "posts/:id/edit", element: <Private><AddPost /></Private> },
-      { path: "posts/:id", element: <FullPost /> }
+      { path: "posts/:id", element: <FullPost /> },
+      { path: "tags/:tag", element: <Tags /> }
     ]
   }
 ]);
