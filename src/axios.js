c600a96aafa7ebde5f4bf44044c4295355ff041f
denis-snitko <denissnitko@gmail.com>
2023-01-14 15:01:32 +0300

    Refactor axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,16 +2,14 @@ import axios from 'axios';
 
 const { REACT_APP_API, REACT_APP_API_PREFIX } = process.env;
 
-console.log(`${REACT_APP_API}${REACT_APP_API_PREFIX}`);
-
 const instance = axios.create({
   baseURL: `${REACT_APP_API}${REACT_APP_API_PREFIX}`,
-})
+});
 
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer ${window.localStorage.getItem('token')}`;
   
   return config;
-})
+});
 
 export default instance;
